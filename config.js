1babb533b56417b097fea6dfd0cb77103da25789
Alona Kosobokova <alonakosobokova@gmail.com>
2017-11-20 17:25:08 -0800

    chore(InitialCommit): Initial Commit

---
+++ config.js
@@ -0,0 +1,18 @@
+(function () {
+  var config = {
+    emailAccount: {
+      username: "idealounge.info@gmail.com",
+      password: 'nodemailer'
+    },
+    freelancers: [{
+      name: "Alona Kosobokova",
+      email: "alonakosobokova@gmail.com"
+    },
+    {
+      name: "Anurag Sahoo",
+      email: "anurags1879@gmail.com"
+    }]
+  };
+  
+  module.exports = config;
+})();
